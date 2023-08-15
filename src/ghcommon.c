eb477c3a083325d6f8e6088920a4f24c620e0cdd
Shane Feek <shane.feek@structuretec.local>
2023-08-15 13:36:38 -0400

    Minor spacing edit
    Signed-off-by: Shane Feek <shane.feek@structuretec.local>

--- src/ghcommon.c
+++ src/ghcommon.c
@@ -1,8 +1,8 @@
 #include "../include/ghcommon.h"
 
 #ifdef _WIN32
-	#include <complex.h>
-	#include <math.h>
+#include <complex.h>
+#include <math.h>
 #endif
 
 int append_string(char **s1, char *s2)
@@ -348,7 +348,8 @@ void pause_for_enter(const char *display)
 
 void free_malloc(void *m)
 {
-	if (m) free(m);
+	if (m)
+		free(m);
 	m = NULL;
 }
 
@@ -585,7 +586,7 @@ int csv_parse(char ***array, char *str, size_t *number_of_fields)
 	char current_character;
 	char **str_array = NULL;
 	int quote = 0;
-	size_t csv_length; 
+	size_t csv_length;
 	int max_field_count = 2; /* Start with two fields as MAX */
 	int *comma_positions = NULL;
 	int *comma_temp = NULL;
@@ -595,9 +596,10 @@ int csv_parse(char ***array, char *str, size_t *number_of_fields)
 	int start_position = 0;
 	int field_length;
 
-	if (str == NULL) return FAIL_PARAMETER;
+	if (str == NULL)
+		return FAIL_PARAMETER;
 
-	csv_length= strlen(str);
+	csv_length = strlen(str);
 
 	/* Allocate memory for the comma position array */
 	if (!(comma_positions = calloc(1, sizeof(int) * max_field_count)))
@@ -735,51 +737,50 @@ void cleanup_csv_strings(char **strArray, size_t numberOfStrings)
 	free_malloc(strArray);
 }
 
-
 #ifdef _WIN32
-  	_Dcomplex add_complex (_Dcomplex n1, _Dcomplex n2)
+_Dcomplex add_complex(_Dcomplex n1, _Dcomplex n2)
+{
+	_Dcomplex num;
+	num._Val[0] = n1._Val[0] + n2._Val[0];
+	num._Val[1] = n1._Val[1] + n2._Val[1];
+
+	return num;
+}
+
+_Dcomplex sub_complex(_Dcomplex n1, _Dcomplex n2)
+{
+	_Dcomplex num;
+	num._Val[0] = n1._Val[0] - n2._Val[0];
+	num._Val[1] = n1._Val[1] - n2._Val[1];
+
+	return num;
+}
+
+_Dcomplex div_complex(_Dcomplex n1, _Dcomplex n2)
+{
+	_Dcomplex num;
+
+	if (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1] == 0.0 || n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1] == 0.0)
 	{
-		_Dcomplex num;
-		num._Val[0] = n1._Val[0] + n2._Val[0];
-		num._Val[1] = n1._Val[1] + n2._Val[1];
+		num._Val[0] = NAN;
+		num._Val[1] = NAN;
 
 		return num;
 	}
 
-	_Dcomplex sub_complex (_Dcomplex n1, _Dcomplex n2)
-	{
-		_Dcomplex num;
-		num._Val[0] = n1._Val[0] - n2._Val[0];
-		num._Val[1] = n1._Val[1] - n2._Val[1];
-	
-		return num;
-	}
+	num._Val[0] = (n1._Val[0] * n2._Val[0] + n1._Val[1] * n2._Val[1]) / (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1]);
+	num._Val[1] = (n1._Val[1] * n2._Val[0] - n1._Val[0] * n2._Val[1]) / (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1]);
 
-	_Dcomplex div_complex (_Dcomplex n1, _Dcomplex n2)
-    {
-        _Dcomplex num;
-        
-		if (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1] == 0.0 || n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1] == 0.0 ) 
-		{
-			num._Val[0] = NAN;
-			num._Val[1] = NAN;
+	return num;
+}
 
-			return num;
-		}
-				
-		num._Val[0] = (n1._Val[0] * n2._Val[0] + n1._Val[1] * n2._Val[1]) / (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1]);
-        num._Val[1] = (n1._Val[1] * n2._Val[0] - n1._Val[0] * n2._Val[1]) / (n2._Val[0] * n2._Val[0] + n2._Val[1] * n2._Val[1]);
-        
-		return num;
-	}
+_Dcomplex mult_complex(_Dcomplex n1, _Dcomplex n2)
+{
+	_Dcomplex num;
 
-	_Dcomplex mult_complex (_Dcomplex n1, _Dcomplex n2)
-	{
-		_Dcomplex num;
+	num._Val[0] = n1._Val[0] * n1._Val[1] - n2._Val[0] * n2._Val[1];
+	num._Val[1] = n1._Val[0] * n2._Val[1] + n2._Val[0] * n1._Val[1];
 
-		num._Val[0] = n1._Val[0] * n1._Val[1] - n2._Val[0] * n2._Val[1];
-		num._Val[1] = n1._Val[0] * n2._Val[1] + n2._Val[0] * n1._Val[1];
-
-		return num;
-	}
+	return num;
+}
 #endif
